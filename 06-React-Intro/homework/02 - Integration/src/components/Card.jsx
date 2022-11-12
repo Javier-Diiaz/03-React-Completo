c665752e9704e5aa3918b304ddd9ad03a40c26a0
Javier-Santiago-Diaz <santiagodiaz0201@gmail.com>
2022-11-12 12:29:13 -0500

    first commit

--- 06-React-Intro/homework/02 - Integration/src/components/Card.jsx
+++ 06-React-Intro/homework/02 - Integration/src/components/Card.jsx
@@ -1,7 +1,7 @@
 export default function Card(props) {
    return (
       <div>
-         <button onClick={onclose}>X</button>
+         <button onClick={props.onclose}>X</button>
          <h2>{props.name}</h2>
          <h2>{props.species}</h2>
          <h2>{props.gender}</h2>
--- 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
+++ 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
@@ -2,17 +2,9 @@ import Card from './Card';
 
 export default function Cards(props) {
    const { characters } = props;
-   var prueba = characters.map((ele) =>
-      <div key={key}>
-         <button onClick={onclose}>X</button>
-         <h2>{ele.name}</h2>
-         <h2>{ele.species}</h2>
-         <h2>{ele.gender}</h2>
-         <img src={ele.image} alt="" />
-      </div>)
    return (
       <div>
-         {prueba}
+
       </div>
    );
 }
