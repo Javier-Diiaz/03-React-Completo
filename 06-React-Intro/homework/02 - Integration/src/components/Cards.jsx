9e26eb6a9d640e64b6e933b8daab2bfa82917454
Javier-Santiago-Diaz <santiagodiaz0201@gmail.com>
2022-11-12 12:49:41 -0500

    first commit

--- 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
+++ 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
@@ -2,9 +2,18 @@ import Card from './Card';
 
 export default function Cards(props) {
    const { characters } = props;
+   var listas = characters.map(ele => {
+      return (
+         <Card key={ele.toString()}>
+            <button onClick={props.onclose}>X</button>
+            <h2>{ele.name}</h2>
+            <h2>{ele.species}</h2>
+            <h2>{ele.gender}</h2>
+            <img src={ele.image} alt="" />
+         </Card>
+      )
+   })
    return (
-      <div>
-
-      </div>
+      <div>{listas}</div>
    );
 }
