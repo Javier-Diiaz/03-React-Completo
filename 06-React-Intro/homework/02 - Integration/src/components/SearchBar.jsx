a75ff29dfe24421d1a0c1be89ad03b558eb5c848
Javier-Santiago-Diaz <santiagodiaz0201@gmail.com>
2022-11-12 18:02:15 -0500

    first commit

--- 06-React-Intro/homework/02 - Integration/src/components/SearchBar.jsx
+++ 06-React-Intro/homework/02 - Integration/src/components/SearchBar.jsx
@@ -2,7 +2,7 @@ export default function SearchBar(props) {
    return (
       <div id='search'>
          <input type='search' />
-         <button onClick={props.onClose}>Agregar</button>
+         <button onClick={props.onSearch}>Agregar</button>
       </div>
    );
 }
