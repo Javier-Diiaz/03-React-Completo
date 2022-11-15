e32c173560ed45fc492f84d93e3b092126d8cdb6
Javier <santiagodiaz0201@gmail.com>
2022-11-14 21:52:08 -0500

    revisando pruebas de estilos

--- 07-React-Estilos/Demo.js
+++ 07-React-Estilos/Demo.js
@@ -1,1 +1,23 @@
 //! Estilos en React
+
+//! React css
+// Al web pack de react hay que agregarle una nueva configuracion para que lea css
+
+//! css Legacy(no recomendable se pisan)
+// ya no usar class en el atributo de una etiqueta  en React por problemas de palabras recervadas en ves de eso se usa className=''
+// con ccs legacy que es el modo que simpre usamos con anterioridad, con web pack en react cuando se renderiza si se tiene id o clases con el mismo nombre se van a pisar
+
+//! inline styling (css-in-js)(cuando crece la app se vuelve algo complicado de seguir)
+//(colocando los stylos en la propia etiqueta html pero llamando las props css como un objeto)
+
+//! Css Modules(Recomensable)
+// cuando compila web pack les cambia el nombre a los nombres de los id y asi no se pisan
+// ademas al ser modularizado todo va a ser mas claro y se cumple la responsabilidad unica de React
+
+
+
+
+
+
+
+
