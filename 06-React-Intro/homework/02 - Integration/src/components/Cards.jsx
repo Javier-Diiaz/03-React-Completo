86fbc5645c229cef470b92525edd5fa8652f2bcc
javier diaz <santiagodiaz0201@gmail.com>
2022-11-24 14:15:52 -0500

    React Intro

--- 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
+++ 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
@@ -2,5 +2,14 @@ import Card from './Card';
 
 export default function Cards(props) {
    const { characters } = props;
-   return <div></div>;
+   const valores = characters.map((char) => {
+      return (<div>
+         <button onClick={() => window.alert('Emulamos que se cierra la card')}></button>
+         <h2>{char.name}</h2>
+         <h2>{char.species}</h2>
+         <h2>{char.gener}</h2>
+         <img src={char.image} alt="" />
+      </div>)
+   })
+   return valores
 }
