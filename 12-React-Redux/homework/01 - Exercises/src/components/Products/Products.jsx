3fd35b3662f5c29e2b46c42cc44121fc9de10cc2
javier diaz <santiagodiaz0201@gmail.com>
2022-12-07 14:47:16 -0500

    react redux exercies completado 100/90

--- 12-React-Redux/homework/01 - Exercises/src/components/Products/Products.jsx
+++ 12-React-Redux/homework/01 - Exercises/src/components/Products/Products.jsx
@@ -1,23 +1,51 @@
 import { connect } from 'react-redux';
 import React from 'react';
+import { getStoreName } from '../../redux/actions/actions'
 import './products.css';
 //El componente Card lo exportamos haciendo destructuring para poder testearlo
-// import Card from '../Card/Card'
+import Card from '../Card/Card'
 
-export function Products() {
+export function Products({ list, storeName, getStoreName }) {
+   React.useEffect(() => {
+      getStoreName()
+   }, [])
+
    return (
       <>
          <div className='productsBg'>
             <h1 className='productsTl'>HENRY MARKET</h1>
+            <h1>{storeName}</h1>
 
             <div className='productsList'>
-               {/* ¡Renderiza aquí todas tus cards! */}
+               {list.map(prod => {
+                  return (
+                     <Card
+                        key={prod.id}
+                        name={prod.name}
+                        price={prod.price}
+                        id={prod.id}
+                     />
+                  )
+               })}
             </div>
          </div>
       </>
    );
 }
 
-export function mapStateToProps() {}
+export function mapStateToProps(state) {
+   return {
+      list: state.list,
+      storeName: state.StoreName
+   }
+}
 
-export default connect(mapStateToProps, null)(Products);
+export function mapDispatchToProps(dispatch) {
+   return {
+      getStoreName: function () {
+         dispatch(getStoreName())
+      }
+   }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Products);
--- 12-React-Redux/homework/01 - Exercises/src/redux/reducer/index.js
+++ 12-React-Redux/homework/01 - Exercises/src/redux/reducer/index.js
@@ -1,7 +1,32 @@
+import { ADD_PRODUCT, DELETE_PRODUCT, GET_STORE_NAME } from '../actions/types'
+
 const initialState = {
     list: [],
+    storeName: ''
 };
 
-const rootReducer = () => { };
+function rootReducer(state = initialState, { type, payload }) {
+    switch (type) {
+        case ADD_PRODUCT:
+            return {
+                ...state,
+                list: [...state.list, payload]
+            }
+        case DELETE_PRODUCT:
+            const filtrado = state.list.filter(ele => ele.id !== payload)
+            return {
+                ...state,
+                list: filtrado
+            }
+        case GET_STORE_NAME:
+            return {
+                ...state,
+                storeName: payload
+
+            }
+        default:
+            return { ...state }
+    }
+}
 
 export default rootReducer;
